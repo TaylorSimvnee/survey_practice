b71b695c830369f761551dc582353ceb4db539a7
TaylorSimvnee <Mstaylorg98@gmail.com>
2022-04-13 18:43:26 -0700

    added solutions

--- src/Survey.java
+++ src/Survey.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Survey {
     public static void main(String[] args) {
@@ -15,5 +17,48 @@ public class Survey {
         -Print the age of the eldest person
          */
 
+        //Declare objects/variables , create your container -> run your program -> print out your results... a good logic flow
+
+        //Global variables or objects needed //on the top - what you need - all variables or objects you need
+        Scanner inputReader = new Scanner(System.in);
+        ArrayList<Participant> participants = new ArrayList<>(); //can do without arrayList but this is a common practice... like using a container for your objects.
+        int youngest = Integer.MAX_VALUE, eldest = 0;
+
+        do {                                                //Run your program
+            System.out.println(SurveyQuestions.askToJoin);
+            String join = inputReader.next();
+
+            if (join.toLowerCase().startsWith("y")){
+
+                Participant.addParticipants();
+
+                //Ask your questions to get name, age, and gender
+                System.out.println(SurveyQuestions.askName);
+                String name = inputReader.next();
+
+                System.out.println(SurveyQuestions.askAge);
+                int age = inputReader.nextInt();
+
+                System.out.println(SurveyQuestions.askGender);
+                String gender = inputReader.next();
+
+                Participant participant = new Participant(name, age , gender);
+                participants.add(participant);
+
+                if (gender.toUpperCase().startsWith("M")) Participant.addMaleParticipants();
+                else Participant.addFemaleParticipants();
+            }
+        }while (Participant.totalNumberOfParticipants < 3);
+
+        for (Participant participant : participants) {
+            System.out.println(participant);
+            if(participant.age > eldest) eldest = participant.age;
+            if(participant.age < youngest) youngest = participant.age;
+        }
+
+        System.out.println("The total number of male participants is = " + Participant.totalNumberOfMaleParticipants); //Print out at the bottom
+        System.out.println("The total number of female participants is = " + Participant.totalNumberOfFemaleParticipants);
+        System.out.println("The age of the youngest person is = " + youngest);
+        System.out.println("The age of the eldest person is = " + eldest);
     }
 }
--- src/SurveyQuestions.java
+++ src/SurveyQuestions.java
@@ -1,11 +1,18 @@
 public class SurveyQuestions {
 
     /*
-    Declare static final instance variables for interview questions below
+    Declare static final instance variables for interview questions below  ---Final bc I DONT want anyone to change my questions
     Would you like to join our survey? (Y/N) -> variable name can be askToJoin
     What is your name? -> variable name can be askName
     How old are you? -> variable name can be askAge
     What is your gender? (F/M) -> -> variable name can be askGender
      */
+    //Should use Enum
+    public static final String askToJoin = "Would you like to join our survey? (Y/N)";
+    public static final String askName = "What is your name?";
+    public static final String askAge = "How old are you?";
+    public static final String askGender = "What is your gender?";
+
+
 
 }
